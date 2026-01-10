d94c16f82c75e9da66c2c914bb5e790fd01cbb6e
askar <askar@e.email>
2026-01-10 18:04:54 +0300

    serve static html

--- main.go
+++ main.go
@@ -2,8 +2,6 @@ package main
 
 import (
 	"fmt"
-	"html/template"
-	"log"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
@@ -11,16 +9,21 @@ import (
 )
 
 func chatHandler(w http.ResponseWriter, r *http.Request) {
-	tmpl, err := template.ParseFiles("html/layout.html")
-	if err != nil {
-		log.Fatal(err)
-	}
-	tmpl.Execute(w, nil)
+	// tmpl, err := template.ParseFiles("html/layout.html")
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// tmpl.Execute(w, nil)
+	http.ServeFile(w, r, "html/layout.html")
 }
 
 func main() {
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
+
+	fs := http.StripPrefix("/static/", http.FileServer(http.Dir("static")))
+	r.Handle("/static/*", fs)
+
 	r.Get("/chat", chatHandler)
 	fmt.Println("starting at :8888...")
 	http.ListenAndServe(":8888", r)
