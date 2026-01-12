ee41a6b4ede056cb5823b4ac639759f6b53142c3
askar <askar@e.email>
2026-01-12 16:40:25 +0300

    store messages in db (sqlite)

--- main.go
+++ main.go
@@ -1,15 +1,16 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
-	"sync"
 	"text/template"
 	"time"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	_ "github.com/mattn/go-sqlite3"
 )
 
 type Message struct {
@@ -28,71 +29,128 @@ type Messages struct {
 }
 
 var (
-	messages    Messages
-	mu          sync.RWMutex
-	lastID      int
 	messageTmpl *template.Template
 )
 
-func chatHandler(w http.ResponseWriter, r *http.Request) {
-	mu.RLock()
-	defer mu.RUnlock()
+func chatHandler(db *sql.DB) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		tmpl, err := template.ParseFiles("html/layout.html", "html/message.html")
+		if err != nil {
+			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+			return
+		}
+		messages, err := getMessages(db, 100)
+		if err != nil {
+			http.Error(w, "Failed to load messages", http.StatusInternalServerError)
+			return
+		}
 
-	tmpl, err := template.ParseFiles("html/layout.html", "html/message.html")
-	if err != nil {
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-		return
-	}
-	err = tmpl.Execute(w, messages)
-	if err != nil {
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-		return
+		err = tmpl.Execute(w, messages)
+		if err != nil {
+			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+			return
+		}
 	}
 }
 
-func messagesHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-		fmt.Println("Method not allowed")
-		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-		return
-	}
+func messagesHandler(db *sql.DB) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodPost {
+			fmt.Println("Method not allowed")
+			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+			return
+		}
 
-	// parse form data
-	err := r.ParseForm()
-	if err != nil {
-		http.Error(w, "Error parsing form", http.StatusBadRequest)
-		return
-	}
+		// parse form data
+		err := r.ParseForm()
+		if err != nil {
+			http.Error(w, "Error parsing form", http.StatusBadRequest)
+			return
+		}
 
-	// extract form values
-	msg := Message{
-		ID:        1,
-		Nickname:  r.FormValue("nickname"),
-		Content:   r.FormValue("content"),
-		CreatedAt: time.Now(),
-	}
+		// extract form values
+		msg := Message{
+			ID:        1,
+			Nickname:  r.FormValue("nickname"),
+			Content:   r.FormValue("content"),
+			CreatedAt: time.Now(),
+		}
 
-	if msg.Content == "" {
-		http.Error(w, "Content field is required", http.StatusBadRequest)
-		return
-	}
-	if msg.Nickname == "" {
-		msg.Nickname = "anonymous"
-	}
+		if msg.Content == "" {
+			http.Error(w, "Content field is required", http.StatusBadRequest)
+			return
+		}
+		if msg.Nickname == "" {
+			msg.Nickname = "anonymous"
+		}
 
-	// process the form data
-	processMessage(msg)
+		// process the form data
+		if err = processMessage(db, msg); err != nil {
+			http.Error(w, "Failed to save message", http.StatusInternalServerError)
+			return
+		}
 
-	w.Header().Set("Content-Type", "text/html")
-	messageTmpl.ExecuteTemplate(w, "msg", msg)
+		w.Header().Set("Content-Type", "text/html")
+		messageTmpl.ExecuteTemplate(w, "msg", msg)
+	}
 }
 
-func processMessage(msg Message) {
-	mu.Lock()
-	defer mu.Unlock()
-	lastID++
-	msg.ID = lastID
-	messages.Msgs = append(messages.Msgs, msg)
+func processMessage(db *sql.DB, msg Message) error {
+	query := `
+	INSERT INTO messages (nickname, content, created_at)
+	VALUES (?, ?, ?)
+	`
+	_, err := db.Exec(
+		query,
+		msg.Nickname,
+		msg.Content,
+		msg.CreatedAt,
+	)
+	return err
+}
+
+func opendb() (*sql.DB, error) {
+	db, err := sql.Open("sqlite3", "./chat.db")
+	if err != nil {
+		return nil, err
+	}
+	db.SetMaxOpenConns(1)
+	return db, nil
+}
+
+func createTables(db *sql.DB) error {
+	query := `
+		CREATE TABLE IF NOT EXISTS messages(
+			id INTEGER PRIMARY KEY AUTOINCREMENT,
+			nickname TEXT NOT NULL,
+			content TEXT NOT NULL,
+			created_at DATETIME NOT NULL
+		);
+	`
+	_, err := db.Exec(query)
+	return err
+}
+
+func getMessages(db *sql.DB, limit int) (Messages, error) {
+	rows, err := db.Query(`
+		SELECT id, nickname, content, created_at
+		FROM messages
+		LIMIT ?
+	`, limit)
+	if err != nil {
+		return Messages{}, err
+	}
+	defer rows.Close()
+
+	var messages Messages
+	for rows.Next() {
+		var m Message
+		if err := rows.Scan(&m.ID, &m.Nickname, &m.Content, &m.CreatedAt); err != nil {
+			return Messages{}, err
+		}
+		messages.Msgs = append(messages.Msgs, m)
+	}
+	return messages, nil
 }
 
 func main() {
@@ -109,8 +167,18 @@ func main() {
 		log.Fatal(err)
 	}
 
-	r.Get("/chat", chatHandler)
-	r.Post("/messages", messagesHandler)
+	db, errdb := opendb()
+	if errdb != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	if err := createTables(db); err != nil {
+		log.Fatal(err)
+	}
+
+	r.Get("/chat", chatHandler(db))
+	r.Post("/messages", messagesHandler(db))
 	fmt.Println("starting on :8888...")
 	http.ListenAndServe(":8888", r)
 }
