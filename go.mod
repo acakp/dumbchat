5c33a937de9f770f5e2d3fafe244f68e4a10c117
askar <askar@e.email>
2026-02-01 17:17:02 +0300

    prepare for websockets

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.25.5
 
 require (
 	github.com/go-chi/chi/v5 v5.2.3
+	github.com/gorilla/websocket v1.5.3
 	github.com/joho/godotenv v1.5.1
 	golang.org/x/crypto v0.47.0
 	modernc.org/sqlite v1.44.3
---
+++ internal/chat/ws.go
@@ -0,0 +1,45 @@
+package chat
+
+import (
+	"github.com/gorilla/websocket"
+)
+
+type Hub struct {
+	clients    map[*Client]bool
+	register   chan *Client
+	unregister chan *Client
+	broadcast  chan Event
+}
+
+type Client struct {
+	conn *websocket.Conn
+	send chan Event
+}
+
+type Event struct {
+	Type string `json:"type"`
+	Data any    `json:"data"`
+}
+
+func (h *Hub) Run() {
+	for {
+		select {
+		case c := <-h.register:
+			h.clients[c] = true
+		case c := <-h.unregister:
+			delete(h.clients, c)
+			close(c.send)
+		case msg := <-h.broadcast:
+			for c := range h.clients {
+				c.send <- msg
+			}
+		}
+	}
+}
+
+func (c *Client) writePump() {
+	defer c.conn.Close()
+	for msg := range c.send {
+		c.conn.WriteJSON(msg)
+	}
+}
